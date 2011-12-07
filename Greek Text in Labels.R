ee4da745a9a9b753e576f89f2e2bb3cf236763c9
Kevin Middleton <kmm@csusb.edu>
2011-12-07 11:07:50 -0800

    Add another Greek text example

--- Greek Text in Labels.R
+++ Greek Text in Labels.R
@@ -3,3 +3,5 @@ theta <- 2.1
 plot(NA, xlim=c(0,1), ylim=c(0,1), xlab=bquote(theta == .(theta)), 
 	ylab=bquote(theta == .(theta)), 
 	main=bquote(paste("Results for ",theta == .(theta))))
+
+plot(1, xlab = expression(sigma[x]))
