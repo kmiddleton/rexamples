8e61d423237da5cb409f032dd896903fe8ac68c4
Kevin Middleton <middletonk@missouri.edu>
2013-06-10 20:26:21 -0500

    Start to put headers on phyl.RMA.test()

--- phyl.RMA.test.R
+++ phyl.RMA.test.R
@@ -1,37 +1,32 @@
 library(smatr)
-
 library(phytools)
-nTaxa <- 50
-set.seed(6)
-x <- rnorm(nTaxa)
-y <- 2 * x + rnorm(nTaxa, sd = 0.1)
-names(x) <- tree$tip.label
-names(y) <- tree$tip.label
 
-tree <- rtree(nTaxa)
-
-fm <- lm(y ~ x)
-coef(fm)[2] / sqrt(summary(fm)$r.squared)
-
-sma(y ~ x, slope.test = 2)
-
-phyl.RMA(x, y, tree = tree, method = "BM")
-
-phyl.RMA(x, y, tree = tree, method = "BM")$RMA.beta
-phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.5), method = "BM")$RMA.beta
-phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.1), method = "BM")$RMA.beta
-phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.001), method = "BM")$RMA.beta
-phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.0001), method = "BM")$RMA.beta
-phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.00001), method = "BM")$RMA.beta
-
-se.rma <- function(rma, x) {
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param rma 
+##' @param x 
+##' @return 
+##' @author Kevin Middleton
+RMA.SE <- function(rma, x) {
   n <- length(rma$resid)
   MSresid <- sqrt(sum(rma$resid^2)) / (n-2)
   se <-  sqrt(MSresid / sum((x - mean(x))^2))
-  se
+  return(se)
 }
 
-slopetest.rma <- function(rma, x, y, b0 = 0) {
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Phylogenetic RMA Slope Test
+##' @param rma 
+##' @param x 
+##' @param y 
+##' @param b0 
+##' @return 
+##' @author Kevin Middleton
+phyl.RMA.slopetest <- function(rma, x, y, b0 = 0) {
   se <- se.ols(x, y)$se.b
   b <- rma$RMA.beta[2]
   t <- (b - b0) / se
@@ -41,6 +36,14 @@ slopetest.rma <- function(rma, x, y, b0 = 0) {
   return(out)
 }
 
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title 
+##' @param x 
+##' @param y 
+##' @return 
+##' @author Kevin Middleton
 se.ols <- function(x, y) {
   fm <- lm(y ~ x)
   summary.fm <- summary(fm)
@@ -48,6 +51,35 @@ se.ols <- function(x, y) {
   se.a <- summary.fm$coefficients[1, 2]
   return(list(se.b = se.b, se.a = se.a))
 }
+
+nTaxa <- 50
+set.seed(6)
+x <- rnorm(nTaxa)
+y <- 2 * x + rnorm(nTaxa, sd = 0.1)
+tree <- rtree(nTaxa)
+names(x) <- tree$tip.label
+names(y) <- tree$tip.label
+
+fm <- lm(y ~ x)
+
+# RMA slope
+coef(fm)[2] / sqrt(summary(fm)$r.squared)
+
+sma(y ~ x, slope.test = 2)
+
+phyl.RMA(x, y, tree = tree, method = "BM")
+
+phyl.RMA(x, y, tree = tree, method = "BM")$RMA.beta
+phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.5), 
+         method = "BM")$RMA.beta
+phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.1), 
+         method = "BM")$RMA.beta
+phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.001), 
+         method = "BM")$RMA.beta
+phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.0001), 
+         method = "BM")$RMA.beta
+phyl.RMA(x, y, tree = compute.brlen(tree, method = "rho", 0.00001), 
+         method = "BM")$RMA.beta
 
 rma <- phyl.RMA(x, y, 
                 tree = compute.brlen(tree, method = "rho", 0.0000001), 
